1fb7cb3200e50fbba5ad751fe474cab88bc6d28f
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:35 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '컨텐츠이용료현금화 희망티켓',
+  siteUrl: 'https://contents-hh-3.pages.dev',
+  siteDescription: '컨텐츠이용료현금화 공식 인증점. 업계 최저 수준 수수료와 24시간 실시간 상담으로 신청 즉시 5분 내 안전 입금!',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
